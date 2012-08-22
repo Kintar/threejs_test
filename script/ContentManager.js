aabd180b9139b2344cfb6719fa375e56651f62a1
Alec Lanter <kintar1900@gmail.com>
2012-08-22 13:49:57 -0400

    Final tweaks, added time-based animation.

--- script/ContentManager.js
+++ script/ContentManager.js
@@ -11,6 +11,9 @@ ContentManager.init = function() {
     ContentManager.Textures.webgl = webgltex;
     
     var cubeGeom = new THREE.CubeGeometry(5,5,5);
-    var cubeMaterial = new THREE.MeshBasicMaterial({ map: ContentManager.Textures.webgl });
+    var cubeMaterial = new THREE.MeshLambertMaterial({ 
+            map: ContentManager.Textures.webgl
+        });
     ContentManager.Models.cube = new THREE.Mesh(cubeGeom, cubeMaterial);
+    ContentManager.Models.cube.useQuaternion = true;
 };
--- script/SceneManager.js
+++ script/SceneManager.js
@@ -1,66 +1,81 @@
-SceneManager = {};
+var SceneManager = SceneManager || {};
 
 SceneManager.setupStats = function() {
     this.stats = new Stats();
     this.stats.domElement.style.position = 'absolute';
     this.stats.domElement.style.bottom = '0px';
     $(document.body).append(this.stats.domElement);
-}
+};
 
 SceneManager.setupRenderer = function() {
     this.renderer = null;
 
-    if (Detector.webgl) {
-        this.renderer = new THREE.WebGLRenderer({
-            antialias: true,
-            preserveDrawingBuffer: true
-        });
-        this.renderer.setClearColorHex(0xBBBBBB, 1);
-    }
-    else {
-        this.renderer = new THREE.CanvasRenderer();
-    }
+    this.renderer = new THREE.WebGLRenderer({
+        antialias: true,
+        preserveDrawingBuffer: true
+    });
+    this.renderer.setClearColorHex(0xBBBBBB, 1);
     
     this.renderer.setSize( window.innerWidth, window.innerHeight );
-    $('#glContainer').append(this.renderer.domElement);
-}
+    this.container.appendChild(this.renderer.domElement);
+};
 
 SceneManager.setupScene = function() {
-    this.scene = new THREE.Scene();
-    this.camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 1, 10000);
-    this.camera.position.set(0, 0, 3);
-    this.scene.add(this.camera);
+    SceneManager.scene = new THREE.Scene();
+    SceneManager.camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 1, 10000);
+    SceneManager.camera.position.set(0, 4, 20);
+    SceneManager.camera.lookAt(new THREE.Vector3(0,0,0));
+    SceneManager.scene.add(SceneManager.camera);
     
     // create a camera contol
-    this.cameraControls = new THREE.TrackballControls(camera, document.getElementById('container'))
-    var light = new THREE.AmbientLight(Math.random() * 0xffffff);
+    //SceneManager.cameraControls = new THREE.TrackballControls(SceneManager.camera, SceneManager.container);
+    
+    // Create some lighting
+    var light = new THREE.AmbientLight(0xffffff);
     this.scene.add(light);
-    var light = new THREE.DirectionalLight(Math.random() * 0xffffff);
+    var light = new THREE.DirectionalLight(0xffffff);
     light.position.set(Math.random(), Math.random(), Math.random()).normalize();
     this.scene.add(light);
     
-}
+    // Add our model
+    this.scene.add(ContentManager.Models.cube);
+    
+    // Set up our time-based rotation
+    SceneManager.angle = 0;
+    SceneManager.rotation = new THREE.Quaternion();
+    SceneManager.YAxis = new THREE.Vector3(0, 1, 0);
+};
     
 SceneManager.init = function () {
     if (!ContentManager.initialied)
         ContentManager.init();
         
+    this.container = document.getElementById('glContainer');
     this.setupRenderer();
     this.setupScene();
     this.setupStats();
-}
+    
+    SceneManager.clock = new THREE.Clock();
+    SceneManager.clock.start();
+};
 
-SceneManager.render = function() {
+SceneManager.update = function() {
+    SceneManager.angle += SceneManager.clock.getDelta();
+    SceneManager.angle %= 360;
+    ContentManager.Models.cube.quaternion.setFromAxisAngle(SceneManager.YAxis, SceneManager.angle);
 
     // update camera controls
-    this.cameraControls.update();
+    if (this.cameraControls)
+        this.cameraControls.update();
+};
 
-    // actually render the scene
+SceneManager.render = function() {
     this.renderer.render(this.scene, this.camera);
-}
+};
 
 SceneManager.animate = function() {
-    requestAnimationFrame(this.animate);
-    this.render();
-    this.stats.update();
-}
+    requestAnimationFrame(SceneManager.animate);
+    SceneManager.update();
+    SceneManager.render();
+    SceneManager.stats.update();
+};
